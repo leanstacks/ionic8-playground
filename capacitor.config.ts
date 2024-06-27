f6e653f30a6edd4a15b84915569a59abe708352d
Matt Warman <mwarman@users.noreply.github.com>
2024-06-27 07:52:10 -0400

    Initial App (#1)
    * initial template
    
    * initial ci workflow

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic8-playground',
+  webDir: 'dist'
+};
+
+export default config;
