1d14cb87e36a680ca81f2e47dd9bc225e1485cb9
Matthew Warman <warmanmr@gmail.com>
2024-07-12 06:29:58 -0400

    cypress

--- cypress/e2e/test.cy.ts
+++ cypress/e2e/test.cy.ts
@@ -1,6 +1,6 @@
-describe('My First Test', () => {
-  it('Visits the app root url', () => {
-    cy.visit('/')
-    cy.contains('#container', 'Ready to create an app?')
-  })
-})
+describe('Home Page', () => {
+  it('Visits the Home Page URL', () => {
+    cy.visit('/tabs/home');
+    cy.get('[data-testid="page-home"]').should('be.visible');
+  });
+});
