cb1f3884eeb69df9ad937d577a3937aa08936aa6
Matthew Warman <warmanmr@gmail.com>
2024-07-13 08:53:20 -0400

    container

---
+++ src/common/components/Content/Container.tsx
@@ -0,0 +1,44 @@
+import { PropsWithChildren } from 'react';
+import classNames from 'classnames';
+
+import { BaseComponentProps } from '../types';
+import classes from './Container.module.scss';
+
+/**
+ * Properties for the `Container` component.
+ * @param {boolean} [fixed] - Indicates if a `fixed` sized container is used.
+ * @see {@link BaseComponentProps}
+ * @see {@link PropsWithChildren}
+ */
+interface ContainerProps extends BaseComponentProps, PropsWithChildren {
+  fixed?: boolean;
+}
+
+/**
+ * The `Container` component controls the width of the enclosed content.
+ *
+ * A standard `Container` is 100% wide.
+ *
+ * A `fixed` `Container` is 100% wide at extra-small and small viewports. It
+ * has a fixed width and is centered at medium, large, and extra-large viewports.
+ *
+ * @param {ContainerProps} props - Component properties.
+ * @returns {JSX.Element} Returns JSX.
+ */
+const Container = ({
+  children,
+  className,
+  fixed = false,
+  testid = 'container',
+}: ContainerProps): JSX.Element => {
+  return (
+    <div
+      className={classNames(classes.container, { [classes.fixed]: fixed }, className)}
+      data-testid={testid}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Container;
