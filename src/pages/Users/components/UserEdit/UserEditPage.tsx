f6e5e616a503858fd50933d6ade6166d62ca2ada
Matthew Warman <warmanmr@gmail.com>
2024-07-21 07:57:35 -0400

    UserEditPage styles

--- src/pages/Users/components/UserEdit/UserEditPage.tsx
+++ src/pages/Users/components/UserEdit/UserEditPage.tsx
@@ -1,5 +1,6 @@
 import { IonContent, IonPage } from '@ionic/react';
 import { useParams } from 'react-router';
+import classNames from 'classnames';
 
 import classes from './UserEditPage.module.scss';
 import { useGetUser } from 'pages/Users/api/useGetUser';
@@ -7,7 +8,6 @@ import Header from 'common/components/Header/Header';
 import Container from 'common/components/Content/Container';
 import PageHeader from 'common/components/Content/PageHeader';
 import Avatar from 'common/components/Icon/Avatar';
-import classNames from 'classnames';
 import LoaderSkeleton from 'common/components/Loader/LoaderSkeleton';
 import UserEditForm from './UserEditForm';
 
@@ -30,23 +30,23 @@ export const UserEditPage = (): JSX.Element => {
   const { data: user } = useGetUser({ userId });
 
   return (
-    <IonPage className={classes['page-user-edit']} data-testid={testid}>
+    <IonPage className={classes.page_user_edit} data-testid={testid}>
       <Header backButton defaultHref="/tabs/users" title={user && user.name} />
 
       <IonContent className="ion-padding">
         <Container fixed>
           <PageHeader
-            className={classNames('ion-hide-md-down', classes['page-header'])}
+            className={classNames('ion-hide-md-down', classes.page_header)}
             title={
               user ? (
-                <div className={classes['title-block']} data-testid={`${testid}-title`}>
+                <div className={classes.title_block} data-testid={`${testid}-title`}>
                   <Avatar value={user.name} className={classes.avatar} />
                   <div>{user.name}</div>
                 </div>
               ) : (
-                <div className={classes['title-block']} data-testid={`${testid}-loader`}>
-                  <LoaderSkeleton widthStyle="2rem" heightStyle="2rem" />
-                  <LoaderSkeleton widthStyle="20rem" heightStyle="2rem" />
+                <div className={classes.title_block} data-testid={`${testid}-loader`}>
+                  <LoaderSkeleton animated widthStyle="2rem" heightStyle="2rem" />
+                  <LoaderSkeleton animated widthStyle="20rem" heightStyle="2rem" />
                 </div>
               )
             }
