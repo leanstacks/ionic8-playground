c679fa29e8586d4dcb23ba96766a96357e7cd9d4
Matthew Warman <warmanmr@gmail.com>
2024-07-30 12:57:20 -0400

    initial IonItemSliding

--- src/pages/Users/components/UserList/UserListItem.tsx
+++ src/pages/Users/components/UserList/UserListItem.tsx
@@ -1,17 +1,30 @@
-import { IonIcon, IonItem, IonLabel } from '@ionic/react';
+import { ComponentPropsWithRef, useRef } from 'react';
+import {
+  IonIcon,
+  IonItem,
+  IonItemOption,
+  IonItemOptions,
+  IonItemSliding,
+  IonLabel,
+  useIonRouter,
+} from '@ionic/react';
+import { create, mail } from 'ionicons/icons';
+import classNames from 'classnames';
 
 import './UserListItem.scss';
+import { BaseComponentProps } from 'common/components/types';
 import { User } from 'common/models/user';
 import Avatar from 'common/components/Icon/Avatar';
-import { mail } from 'ionicons/icons';
 
 /**
  * Properties for the `UserListItem` component.
- * @param {string} [lines] - See `lines` from `IonItem`.
  * @param {User} user - A `User` object.
- * @see {@link HTMLIonItemElement}
+ * @see {@link BaseComponentProps}
+ * @see {@link IonItem}
  */
-interface UserListItemProps extends Pick<HTMLIonItemElement, 'lines'> {
+interface UserListItemProps
+  extends BaseComponentProps,
+    Pick<ComponentPropsWithRef<typeof IonItem>, 'lines'> {
   user: User;
 }
 
@@ -23,32 +36,47 @@ interface UserListItemProps extends Pick<HTMLIonItemElement, 'lines'> {
  * @param {UserListItemProps} props - Component properties.
  * @returns JSX
  */
-const UserListItem = ({ lines, user }: UserListItemProps): JSX.Element => {
-  const testid = `list-item-user-${user.id}`;
+const UserListItem = ({ className, lines, testid, user }: UserListItemProps): JSX.Element => {
+  const testIdentifier = testid ?? `list-item-user-${user.id}`;
+  const router = useIonRouter();
+  const ionSlidingRef = useRef<HTMLIonItemSlidingElement>(null);
+
+  const doEdit = () => {
+    ionSlidingRef.current?.close();
+    router.push(`/tabs/users/${user.id}/edit`);
+  };
 
   return (
-    <IonItem
-      className="list-item-user"
-      data-testid={testid}
-      routerLink={`/tabs/users/${user.id}`}
-      lines={lines}
-      detail
-    >
-      <Avatar value={user.name} />
-      <IonLabel>
-        <div className="content-row primary">
-          <div className="name" data-testid={`${testid}-name`}>
-            {user.name}
+    <IonItemSliding ref={ionSlidingRef} data-testid={testIdentifier}>
+      <IonItem
+        className={classNames('list-item-user', className)}
+        routerLink={`/tabs/users/${user.id}`}
+        lines={lines}
+        detail
+      >
+        <Avatar value={user.name} />
+        <IonLabel>
+          <div className="content-row primary">
+            <div className="name" data-testid={`${testIdentifier}-name`}>
+              {user.name}
+            </div>
           </div>
-        </div>
-        <div className="content-row secondary">
-          <div>
-            <IonIcon icon={mail} />
-            <div data-testid={`${testid}-email`}>{user.email}</div>
+          <div className="content-row secondary">
+            <div>
+              <IonIcon icon={mail} />
+              <div data-testid={`${testIdentifier}-email`}>{user.email}</div>
+            </div>
           </div>
-        </div>
-      </IonLabel>
-    </IonItem>
+        </IonLabel>
+      </IonItem>
+
+      <IonItemOptions>
+        <IonItemOption onClick={() => doEdit()}>
+          <IonIcon icon={create} slot="start" />
+          Edit
+        </IonItemOption>
+      </IonItemOptions>
+    </IonItemSliding>
   );
 };
 
