5623871fc62872d0ea58e2d0c194403dbf188194
Matthew Warman <warmanmr@gmail.com>
2024-08-09 16:15:04 -0400

    auth provider; sign out; get user tokens

---
+++ src/common/api/useGetUserTokens.ts
@@ -0,0 +1,43 @@
+import { useQuery } from '@tanstack/react-query';
+import dayjs from 'dayjs';
+
+import { UserTokens } from 'common/models/auth';
+import { QueryKey, StorageKey } from 'common/utils/constants';
+import storage from 'common/utils/storage';
+
+/**
+ * An API hook which fetches tokens from the application Identity Provider, IdP.
+ * This hook contains example logic which should be replaced with an actual
+ * IdP integration.
+ * @returns Returns a `UseQueryResult` with `UserTokens` data.
+ */
+export const useGetUserTokens = () => {
+  const getUserTokens = async (): Promise<UserTokens> => {
+    // TODO: replace with logic to fetch tokens from your IdP
+    return new Promise((resolve, reject) => {
+      const storedTokens = storage.getItem(StorageKey.UserTokens);
+
+      if (storedTokens) {
+        // tokens found
+        const tokens = JSON.parse(storedTokens) as unknown as UserTokens;
+        const now = dayjs();
+        if (now.isBefore(tokens.expires_at)) {
+          // tokens not expired
+          return resolve(tokens);
+        } else {
+          // tokens expired
+          return reject(new Error('Tokens expired.'));
+        }
+      }
+
+      // tokens not found
+      return reject(new Error('Tokens not found.'));
+    });
+  };
+
+  return useQuery({
+    queryKey: [QueryKey.UserTokens],
+    queryFn: () => getUserTokens(),
+    retry: 0,
+  });
+};
---
+++ src/common/providers/AuthProvider.tsx
@@ -0,0 +1,63 @@
+import { IonContent, IonPage } from '@ionic/react';
+import { createContext, PropsWithChildren } from 'react';
+import { QueryObserverBaseResult } from '@tanstack/react-query';
+
+import './AuthProvider.scss';
+import { UserTokens } from 'common/models/auth';
+import { useGetUserTokens } from 'common/api/useGetUserTokens';
+import LoaderSpinner from 'common/components/Loader/LoaderSpinner';
+
+/**
+ * The `value` provided by the `AuthContext`.
+ */
+export interface AuthContextValue {
+  isAuthenticated: boolean;
+  userTokens?: UserTokens;
+  refetchUserTokens?: () => Promise<QueryObserverBaseResult<UserTokens, Error>>;
+}
+
+/**
+ * The default `AuthContext` value.
+ */
+const DEFAULT_CONTEXT_VALUE: AuthContextValue = {
+  isAuthenticated: false,
+};
+
+/**
+ * The `AuthContext` instance.
+ */
+export const AuthContext = createContext<AuthContextValue>(DEFAULT_CONTEXT_VALUE);
+
+/**
+ * The `AuthProvider` component creates and provides access to the `AuthContext`
+ * value.
+ * @param {PropsWithChildren} props - Component properties.
+ * @returns {JSX.Element} JSX
+ */
+const AuthProvider = ({ children }: PropsWithChildren): JSX.Element => {
+  const { data: userTokens, isPending, isSuccess, refetch: refetchUserTokens } = useGetUserTokens();
+
+  const value: AuthContextValue = {
+    isAuthenticated: isSuccess,
+    userTokens: userTokens,
+    refetchUserTokens,
+  };
+  console.log(`AuthProvider::value::${JSON.stringify(value)}`);
+
+  const isReady = !isPending;
+
+  return (
+    <AuthContext.Provider value={value}>
+      {isReady && <>{children}</>}
+      {!isReady && (
+        <IonPage className="provider-auth">
+          <IonContent fullscreen>
+            <LoaderSpinner />
+          </IonContent>
+        </IonPage>
+      )}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
