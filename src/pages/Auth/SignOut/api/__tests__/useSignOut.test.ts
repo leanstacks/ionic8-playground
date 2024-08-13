e6d8fa1ab9ad278c6bdd1787a28288ca268c02f8
Matthew Warman <warmanmr@gmail.com>
2024-08-13 10:51:18 -0400

    tests

---
+++ src/pages/Auth/SignOut/api/__tests__/useSignOut.test.ts
@@ -0,0 +1,48 @@
+import { describe, expect, it, vi } from 'vitest';
+
+import { renderHook, waitFor } from 'test/test-utils';
+
+import { useSignOut } from '../useSignOut';
+import storage from 'common/utils/storage';
+
+describe('useSignOut', () => {
+  it('should render hook sucessufully', async () => {
+    // ARRANGE
+    let isSuccess = false;
+    const { result } = renderHook(() => useSignOut());
+    await waitFor(() => expect(result.current).not.toBeNull());
+
+    // ACT
+    result.current.mutate(undefined, {
+      onSuccess: () => {
+        isSuccess = true;
+      },
+    });
+    await waitFor(() => expect(result.current.isSuccess).toBe(true));
+
+    // ASSERT
+    expect(isSuccess).toBe(true);
+  });
+
+  it('should return error when sign out fails', async () => {
+    // ARRANGE
+    const removeItemSpy = vi.spyOn(storage, 'removeItem');
+    removeItemSpy.mockImplementation(() => {
+      throw new Error('test');
+    });
+    let isError = false;
+    const { result } = renderHook(() => useSignOut());
+    await waitFor(() => expect(result.current).not.toBeNull());
+
+    // ACT
+    result.current.mutate(undefined, {
+      onError: () => {
+        isError = true;
+      },
+    });
+    await waitFor(() => expect(result.current.isError).toBe(true));
+
+    // ASSERT
+    expect(isError).toBe(true);
+  });
+});
