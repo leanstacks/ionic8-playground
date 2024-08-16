234ea288588164ee62377a8e9513186c573ea789
Matt Warman <mwarman@users.noreply.github.com>
2024-08-16 14:21:55 -0400

    42 Sign in help (#54)
    * icon color
    
    * sign in help popover
    
    * pr fixes

--- src/common/components/Icon/Icon.tsx
+++ src/common/components/Icon/Icon.tsx
@@ -1,4 +1,5 @@
 import { ComponentPropsWithoutRef } from 'react';
+import { IonText } from '@ionic/react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { IconProp } from '@fortawesome/fontawesome-svg-core';
 import {
@@ -28,7 +29,8 @@ import { BaseComponentProps } from '../types';
  */
 export interface IconProps
   extends BaseComponentProps,
-    Omit<ComponentPropsWithoutRef<typeof FontAwesomeIcon>, 'icon'> {
+    Omit<ComponentPropsWithoutRef<typeof FontAwesomeIcon>, 'color' | 'icon'>,
+    Pick<ComponentPropsWithoutRef<typeof IonText>, 'color'> {
   icon: IconName;
 }
 
@@ -79,15 +81,23 @@ const icons: Record<IconName, IconProp> = {
  * @returns {JSX.Element} JSX
  * @see {@link FontAwesomeIcon}
  */
-const Icon = ({ className, icon, testid = 'icon', ...iconProps }: IconProps): JSX.Element => {
+const Icon = ({
+  className,
+  color,
+  icon,
+  testid = 'icon',
+  ...iconProps
+}: IconProps): JSX.Element => {
   const faIcon = icons[icon];
   return (
-    <FontAwesomeIcon
-      className={classNames('icon', className)}
-      icon={faIcon}
-      {...iconProps}
-      data-testid={testid}
-    />
+    <IonText color={color} data-testid={testid}>
+      <FontAwesomeIcon
+        className={classNames('icon', className)}
+        icon={faIcon}
+        {...iconProps}
+        data-testid={`${testid}-icon`}
+      />
+    </IonText>
   );
 };
 
