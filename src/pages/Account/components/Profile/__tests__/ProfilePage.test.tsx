df37e5c1a85879c0fa259df1861c6c9618b4febb
Matthew Warman <warmanmr@gmail.com>
2024-08-23 13:25:29 -0400

    tests

---
+++ src/pages/Account/components/Profile/__tests__/ProfilePage.test.tsx
@@ -0,0 +1,54 @@
+import { describe, expect, it, vi } from 'vitest';
+
+import { render, screen } from 'test/test-utils';
+import * as UseGetCurrentUser from 'common/api/useGetCurrentUser';
+
+import ProfilePage from '../ProfilePage';
+import { UseQueryResult } from '@tanstack/react-query';
+import { User } from 'common/models/user';
+import { userFixture1 } from '__fixtures__/users';
+
+describe('ProfilePage', () => {
+  const useGetCurrentUserSpy = vi.spyOn(UseGetCurrentUser, 'useGetCurrentUser');
+
+  it('should render successfully', async () => {
+    // ARRANGE
+    useGetCurrentUserSpy.mockReturnValueOnce({
+      data: userFixture1,
+      isLoading: false,
+      isError: false,
+      isSuccess: true,
+    } as unknown as UseQueryResult<User, Error>);
+    render(<ProfilePage />);
+    await screen.findByTestId('page-profile');
+
+    // ASSERT
+    expect(screen.getByTestId('page-profile')).toBeDefined();
+  });
+
+  it('should render loading state', async () => {
+    // ARRANGE
+    useGetCurrentUserSpy.mockReturnValueOnce({ isLoading: true } as unknown as UseQueryResult<
+      User,
+      Error
+    >);
+    render(<ProfilePage />);
+    await screen.findByTestId('page-profile-loading');
+
+    // ASSERT
+    expect(screen.getByTestId('page-profile-loading')).toBeDefined();
+  });
+
+  it('should render error state', async () => {
+    // ARRANGE
+    useGetCurrentUserSpy.mockReturnValueOnce({
+      isError: true,
+      isLoading: false,
+    } as unknown as UseQueryResult<User, Error>);
+    render(<ProfilePage />);
+    await screen.findByTestId('page-profile-error');
+
+    // ASSERT
+    expect(screen.getByTestId('page-profile-error')).toBeDefined();
+  });
+});
