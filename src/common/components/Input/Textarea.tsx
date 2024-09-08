ae2ffbf0a1e9c378f6d1bfe687de292193787531
Matthew Warman <warmanmr@gmail.com>
2024-09-08 06:30:04 -0400

    forwardRef support

--- src/common/components/Input/Textarea.tsx
+++ src/common/components/Input/Textarea.tsx
@@ -1,5 +1,5 @@
 import { IonTextarea, TextareaCustomEvent } from '@ionic/react';
-import { ComponentPropsWithoutRef } from 'react';
+import { ComponentPropsWithoutRef, forwardRef } from 'react';
 import classNames from 'classnames';
 
 import { PropsWithTestId } from '../types';
@@ -19,34 +19,37 @@ interface TextareaProps
  * The `Textarea` component renders a standardized `IonTextarea` which is
  * integrated with Formik.
  *
+ * Optionally accepts a forwarded `ref` which allows the parent to manipulate
+ * the textarea, performing actions programmatically such as giving focus.
+ *
  * @param {TextareaProps} props - Component properties.
  * @returns {JSX.Element} JSX
  */
-const Textarea = ({
-  className,
-  testid = 'textarea',
-  ...textareaProps
-}: TextareaProps): JSX.Element => {
-  const [field, meta, helpers] = useField(textareaProps.name);
+const Textarea = forwardRef<HTMLIonTextareaElement, TextareaProps>(
+  ({ className, testid = 'textarea', ...textareaProps }: TextareaProps, ref): JSX.Element => {
+    const [field, meta, helpers] = useField(textareaProps.name);
 
-  return (
-    <IonTextarea
-      className={classNames(
-        'ls-textarea',
-        className,
-        { 'ion-touched': meta.touched },
-        { 'ion-invalid': meta.error },
-        { 'ion-valid': meta.touched && !meta.error },
-      )}
-      onIonInput={async (e: TextareaCustomEvent) => {
-        await helpers.setValue(e.detail.value);
-      }}
-      data-testid={testid}
-      {...field}
-      {...textareaProps}
-      errorText={meta.error}
-    ></IonTextarea>
-  );
-};
+    return (
+      <IonTextarea
+        className={classNames(
+          'ls-textarea',
+          className,
+          { 'ion-touched': meta.touched },
+          { 'ion-invalid': meta.error },
+          { 'ion-valid': meta.touched && !meta.error },
+        )}
+        onIonInput={async (e: TextareaCustomEvent) => {
+          await helpers.setValue(e.detail.value);
+        }}
+        data-testid={testid}
+        {...field}
+        {...textareaProps}
+        errorText={meta.error}
+        ref={ref}
+      ></IonTextarea>
+    );
+  },
+);
+Textarea.displayName = 'Textarea';
 
 export default Textarea;
