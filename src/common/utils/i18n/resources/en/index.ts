c80dcdf1d5f4536c20ab9c501cfae94d40999209
Matthew Warman <warmanmr@gmail.com>
2024-10-06 07:20:02 -0400

    account page i18n

--- src/common/utils/i18n/resources/en/index.ts
+++ src/common/utils/i18n/resources/en/index.ts
@@ -1,5 +1,6 @@
+import account from './account.json';
 import auth from './auth.json';
 import common from './common.json';
 import home from './home.json';
 
-export default { auth, common, home };
+export default { account, auth, common, home };
