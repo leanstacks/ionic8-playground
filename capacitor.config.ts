9eb2dfd6e6275f32d9ed1c21e1a58adcc316e695
Matthew Warman <warmanmr@gmail.com>
2024-10-14 11:57:35 -0400

    splash screen capacitor

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,17 @@
+/// <reference types="@capacitor/splash-screen" />
+
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'net.leanstacks.ionic8',
   appName: 'Ionic Playground 8',
   webDir: 'dist',
+  // plugins: {
+  //   SplashScreen: {
+  //     launchAutoHide: true,
+  //     launchShowDuration: 3000,
+  //   },
+  // },
 };
 
 export default config;
